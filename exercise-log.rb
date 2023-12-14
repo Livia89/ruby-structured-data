6af19df4ed32cd88d711155afd2cad81a38373b5
Livia89 <liviapriscilla1989@gmail.com>
2023-12-14 22:37:09 +0000

    total all contacts of ip per year

--- exercise-log.rb
+++ exercise-log.rb
@@ -1,9 +1,4 @@
-#1º
-#Numero contactos por ip diferentes.
-#(Contar quantas vezes um determinado ip contactou o servidor.)
-#(Total de Ips que contactaram o servidor)
-
-#exemplo:
+#example:
 #188.83.64.154 - 10
 #63.141.251.236 - 5
 #Total 2 ip's
@@ -20,12 +15,48 @@ ips = {}
 ips_year = {}
 
 for row in rows
-  auxArr = row.gsub(/ - - | - |"/, '|').split("|")
-  arrayData.append(auxArr[0], auxArr[1], auxArr[2], auxArr[3].strip)
+  rowArr = row.gsub(/ - - | - |"/, '|').split("|")
 
-  year =  auxArr[1].gsub(/[\/|:]/, ',').split(",")[2]
+  ip, date, req, error = rowArr[0], rowArr[1], rowArr[2], rowArr[3].strip
 
-  ips[auxArr[0]] = 0 unless !ips.empty?
-  ips.include?(auxArr[0]) ? (ips[auxArr[0]] = ips[auxArr[0]] + 1) : (ips[auxArr[0]] = 1)
+  yearReq =  date.gsub(/[\/|:]/, ',').split(",")[2]
+
+  ips[ip] = 0 unless !ips.empty?
+  ips.include?(ip) ? (ips[ip] = ips[ip] + 1) : (ips[ip] = 1)
+
+  #cria uma nova posição ao menos que a chave do ano ja exista
+  ips_year[yearReq] = {} unless ips_year.include?(yearReq)
+
+  ips_year.each{|year, list|
+
+    if(year == yearReq)
+      ips_year[year].include?(ip) ?
+      (list.merge!(ip => ips_year[year][ip] + 1)) :
+      (list.merge!(ip => 1))
+    end
+  }
 
 end
+
+puts "Total de Ips que contactaram o servidor \n\n"
+ips.each do |key, value|
+  puts "#{key} => #{value}"
+end
+
+
+puts "*------------*" * 8
+
+#example:
+#2021 - 188.83.64.154 - 10
+#2021 - 63.141.251.236 - 5
+#2022 - 188.83.64.154 - 25
+
+puts "\nNumero contactos por ip diferentes/por ano \n\n"
+ips_year.each do |year, list|
+  list.each do |ip, amount|
+    print "#{year} - #{ip} => #{amount}"
+    puts ("\n")
+  end
+end
+
+puts "*------------*" * 8
