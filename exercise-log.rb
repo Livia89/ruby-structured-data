bff254a811845a6200145e218b55ddb2ff9b62d2
Livia89 <liviapriscilla1989@gmail.com>
2023-12-14 23:55:03 +0000

    Ips requisition that resulted 4** errors + improvements

--- exercise-log.rb
+++ exercise-log.rb
@@ -1,8 +1,3 @@
-#example:
-#188.83.64.154 - 10
-#63.141.251.236 - 5
-#Total 2 ip's
-
 #open the file
 fileOpened = File.open("teambuilder.log")
 data = fileOpened.read
@@ -10,39 +5,46 @@ fileOpened.close
 
 rows = data.split("\n")
 
-arrayData = []
 ips = {}
 ips_year = {}
-
+reqErrors = {}
+reqTotal = 0
 rows.each do |row|
   rowArr = row.gsub(/ - - | - |"/, '|').split("|")
+  ip, date, req, error = rowArr[0], rowArr[1], rowArr[2], rowArr[3].strip.split[0].to_s #data setted to variables
+  yearRow =  date.gsub(/[\/|:]/, ',').split(",")[2]
 
-  ip, date, req, error = rowArr[0], rowArr[1], rowArr[2], rowArr[3].strip
-
-  yearReq =  date.gsub(/[\/|:]/, ',').split(",")[2]
-
+  # ip hash started & add ips made req to the server
   ips[ip] = 0 unless !ips.empty?
   ips.include?(ip) ? (ips[ip] = ips[ip] + 1) : (ips[ip] = 1)
 
-  #cria uma nova posição ao menos que a chave do ano ja exista
-  ips_year[yearReq] = {} unless ips_year.include?(yearReq)
-
+  # new position created unless the year key already exists &
+  ips_year[yearRow] = {} unless ips_year.include?(yearRow)
+  # hash iteration over to count the number of ips requisitions to the server
   ips_year.each{|year, list|
-
-    if(year == yearReq)
+    if(year == yearRow)
       ips_year[year].include?(ip) ?
       (list.merge!(ip => ips_year[year][ip] + 1)) :
       (list.merge!(ip => 1))
     end
   }
 
+  #ips requisition verification indicating 4** errors
+  error[0].to_i == 4 && reqErrors.merge!({ip => "#{error} - #{req}"})
+
 end
+
+#example:
+#188.83.64.154 - 10
+#63.141.251.236 - 5
+#Total 2 ip's
 
 puts "\n\nTotal IPs that made requests to the server\n\n"
 ips.each do |key, value, index|
   puts "#{key} => #{value}"
+  reqTotal += value
 end
-
+puts "\nTotal: #{reqTotal} ip's \n\n"
 
 print "*------------*" * 8
 
@@ -58,5 +60,15 @@ ips_year.each do |year, list|
     puts ("\n")
   end
 end
-
+puts  "\n\n"
 puts "*------------*" * 8
+puts  "\n\n"
+
+#example:
+#188.83.64.154 => 404 - GET /robots.txt HTTP/1.1
+#188.83.64.154 => 404 - GET /sitemap.xml HTTP/1.1
+
+puts "\n\nIps requisition that resulted 4** errors\n\n"
+reqErrors.each do |ip, info|
+  puts "#{ip} => #{info}"
+end
