89103d7c2e5bb2ff33b34198f896ae6c2bdae0bd
Lívia Gomes <liviapriscilla1989@gmail.com>
2024-01-09 12:06:01 +0000

    Add files via upload

--- event.rb
+++ event.rb
@@ -2,17 +2,19 @@ require "win32/eventLog"
 include Win32
 
 log = Win32::EventLog.open("System")
-
+result = ""
 puts "Records number: #{log.total_records}"
-
+result = result + "Records number: #{log.total_records}"
 log.read { |record|
-  if record.event_id == 6013 # 6013 - system uptime
-    date = record.time_generated
-    seconds = record.description.split
-    hours = seconds[4].to_i / 3600
-    days = hours/24
-    hours = hours-days * 24
 
-    puts "The computer was turned on in #{date}: #{days} days - #{hours} hours"
-  end
+    if record.event_id == 6013 # 6013 - system uptime
+      seconds = record.description.split
+      hours = seconds[4].to_i / 3600
+      days = hours/24
+      hours = hours-days * 24
+
+      puts "In #{record.time_generated} the computer was turned on in #{record.time_generated}: #{days} days - #{hours} hours"
+      result = result + "\nIn #{record.time_generated} the computer was turned on in #{record.time_generated}: #{days} days - #{hours} hours"
+    end
 }
+File.open("events-log.txt", "w") { |f| f.write result }
